5ef5dcf0d31b0047c4eb93c8ad2837971acbb89e
Nusrat Shams <39095336+nusratshams@users.noreply.github.com>
2018-07-23 21:06:22 +0600

    Add files via upload

---
+++ FlexServo.ino
@@ -0,0 +1,18 @@
+
+#include <Servo.h>
+
+Servo myservo; 
+
+int flexpin = 0;
+int val;
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  val = analogRead(flexpin);
+  val = map(val, 200, 400, 0, 180);
+  myservo.write(val);
+  delay(15);
+}
